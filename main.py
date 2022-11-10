77dfbad4b4b243133202faab64be58ccc6cc5f77
greatcurassow <92639419+greatcurassow@users.noreply.github.com>
2022-11-09 19:28:25 -0600

    Updated with BertForNextSentencePrediction

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import uuid
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
+from torch.nn.functional import softmax
+from transformers import BertForNextSentencePrediction, BertTokenizer
+
 import numpy as np
 import pandas as pd
 
@@ -38,6 +41,27 @@ async def getQuestions():
   return questions
 
 @app.post("/answers")
+def computeScore2(answers: List[Answer]):
+  # load pretrained model and a pretrained tokenizer
+  model = BertForNextSentencePrediction.from_pretrained('bert-base-cased')
+  tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+  score=0
+  for answer in answers:
+    encoded = tokenizer.encode_plus(answer.questionText, text_pair=answer.answerText, return_tensors='pt')
+    #print(encoded)
+    seq_relationship_logits = model(**encoded)[0]
+    probs = softmax(seq_relationship_logits, dim=1)
+    print(seq_relationship_logits)
+    print(probs)
+    sim_score=probs[0][0].item()
+    print('sim_score',sim_score)
+    if(sim_score>0.7):
+      score=score+1
+    print('score:',score)
+  finalScore = (score/(len(answers)))*100
+  return finalScore
+
+@app.post("/answersOld")
 def computeScore(answers: List[Answer]):
   model = SentenceTransformer('bert-base-nli-mean-tokens')
   score=0
